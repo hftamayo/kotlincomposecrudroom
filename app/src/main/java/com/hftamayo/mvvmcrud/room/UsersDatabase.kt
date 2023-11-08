8dfb3178fb0c16594075b7ad0f4287ba89394f35
hftamayo <hftamayo@gmail.com>
2023-11-08 12:53:14 -0600

    users database part1

---
+++ app/src/main/java/com/hftamayo/mvvmcrud/room/UsersDatabase.kt
@@ -0,0 +1,14 @@
+package com.hftamayo.mvvmcrud.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.hftamayo.mvvmcrud.models.Users
+
+@Database(
+    entities = [Users::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class UsersDatabase: RoomDatabase() {
+
+}
