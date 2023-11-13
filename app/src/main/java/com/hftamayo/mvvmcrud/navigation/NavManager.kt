c4033e071270c9b490746eaa8274b71b1fe08289
hftamayo <hftamayo@gmail.com>
2023-11-13 11:16:01 -0600

    app router part 2

--- app/src/main/java/com/hftamayo/mvvmcrud/navigation/NavManager.kt
+++ app/src/main/java/com/hftamayo/mvvmcrud/navigation/NavManager.kt
@@ -1,9 +1,11 @@
 package com.hftamayo.mvvmcrud.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.hftamayo.mvvmcrud.viewmodels.UsersViewModel
 
 @Composable
@@ -11,6 +13,26 @@ fun NavManager(viewModel: UsersViewModel){
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = "home") {
-        composable("home")
+        composable("home") {
+            HomeView(navController, viewModel)
+        }
+
+        composable("add") {
+            AddView(navController, viewModel)
+        }
+
+        composable("edit/{id}/{user}/{email}", arguments = listOf(
+            navArgument("id"){ type = NavType.IntType },
+            navArgument("user"){ type = NavType.StringType },
+            navArgument("email"){ type = NavType.StringType }
+        )) {
+            EditView(
+                navController,
+                viewModel,
+                it.arguments!!.getInt("id"),
+                it.arguments!!.getString("user"),
+                it.arguments!!.getString("email")
+            )
+        }
     }
 }
