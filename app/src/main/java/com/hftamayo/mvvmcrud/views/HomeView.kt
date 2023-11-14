28e012a7b5cca5e390bfae2e1947954138aea4ae
hftamayo <hftamayo@gmail.com>
2023-11-14 12:19:37 -0600

    Home View part2

--- app/src/main/java/com/hftamayo/mvvmcrud/views/HomeView.kt
+++ app/src/main/java/com/hftamayo/mvvmcrud/views/HomeView.kt
@@ -1,15 +1,26 @@
 package com.hftamayo.mvvmcrud.views
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import com.hftamayo.mvvmcrud.viewmodels.UsersViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeView(navController: NavController, viewModel: ViewModel) {
+fun HomeView(navController: NavController, viewModel: UsersViewModel) {
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -25,13 +36,34 @@ fun HomeView(navController: NavController, viewModel: ViewModel) {
             FloatingActionButton(
                 onClick = {
                     navController.navigate("add")
-                }
-
+                },
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = Color.White
             ) {
-
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
             }
         }
     ) {
-
+        ContentHomeView(it, navController, viewModel)
     }
+}
+
+@Composable
+fun ContentHomeView(it: PaddingValues, navController: NavController, viewModel: UsersViewModel){
+    val state = viewModel.state
+
+    Column(
+        modifier = Modifier.padding(it)
+    ) {
+        LazyColumn{
+            items(state.listUsers){
+                Box(
+                    modifier = Modifier.padding(8.dp)
+                ){
+
+                }
+            }
+        }
+    }
+
 }
