3e66486b1c09cdea41051ab984e6b4a627d7d7a9
hftamayo <hftamayo@gmail.com>
2023-11-16 09:57:44 -0600

    updating user model

--- app/src/main/java/com/hftamayo/mvvmcrud/models/Users.kt
+++ app/src/main/java/com/hftamayo/mvvmcrud/models/Users.kt
@@ -8,10 +8,8 @@ import androidx.room.PrimaryKey
 data class Users(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-    @ColumnInfo("firstname")
-    val firstname: String,
-    @ColumnInfo("lastname")
-    val lastname: String,
+    @ColumnInfo("user")
+    val user: String,
     @ColumnInfo("email")
     val email: String
 )
