ce5835e5bf5e0095c79021caf5ed566290096fdb
hftamayo <hftamayo@gmail.com>
2023-11-17 11:15:27 -0600

    finishing Add View form

--- app/src/main/java/com/hftamayo/mvvmcrud/views/AddView.kt
+++ app/src/main/java/com/hftamayo/mvvmcrud/views/AddView.kt
@@ -1,15 +1,23 @@
 package com.hftamayo.mvvmcrud.views
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.hftamayo.mvvmcrud.models.Users
 import com.hftamayo.mvvmcrud.viewmodels.UsersViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddView (navController: NavController, viewModel: UsersViewModel){
+fun AddView(navController: NavController, viewModel: UsersViewModel) {
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -22,13 +30,60 @@ fun AddView (navController: NavController, viewModel: UsersViewModel){
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }
                     ) {
-                        Icon(imageVector = , contentDescription = )
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Go Back",
+                            tint = Color.White
+                        )
 
                     }
                 }
             )
         }
     ) {
+        ContentAddView(it, navController, viewModel)
+    }
+}
 
+@Composable
+fun ContentAddView(it: PaddingValues, navController: NavController, viewModel: UsersViewModel) {
+    var user by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .padding(it)
+            .padding(top = 30.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = user,
+            onValueChange = { user = it },
+            label = { Text(text = "User") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+                .padding(bottom = 15.dp)
+        )
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(text = "Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+                .padding(bottom = 15.dp)
+        )
+
+        Button(
+            onClick = {
+                val user = Users(user = user, email = email)
+                viewModel.addUser(user)
+                navController.popBackStack()
+            }
+        ) {
+            Text(text = "Add")
+        }
     }
 }
